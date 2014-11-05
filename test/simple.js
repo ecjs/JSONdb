91c087328311ffdc9779a02a84517e54b58b850b
Mark Harrell <mark@mjharrell.com>
2014-11-05 10:03:33 -0800

    superagent instead of chai-http

--- test/simple.js
+++ test/simple.js
@@ -2,15 +2,32 @@ var chai = require('chai');
 var chaihttp = require('chai-http');
 var expect = require('chai').expect;
 var app = require('../index.js');
+var superagent = require('superagent');
+
 
 chai.use(chaihttp);
 
+describe('Should accept JSON', function() {
+  it('Sending JSON should write a file', function() {
+    var agent = superagent.agent();
+    agent
+    .post('http://localhost:3000/api/test')
+    .send({message: 'hey test worked!'})
+    .set('Accept', 'application/json')
+    .end(function(err, res) {
+      if (err) console.log('error! : ' + err);
+      expect(res).to.be.json;
+    });
+  });
+});
+
 describe('Simple JSON API', function() {
-  it('should send the local time', function() {
-    chai.request('http://localhost:3000')
-  .get('/api')
+  it('Response should be json', function() {
+    var agent = superagent.agent();
+    agent
+  .get('http://localhost:3000/api/test')
   .end(function(err, res) {
-     expect(res).to.be.json;
+     expect(res).to.have.status(200);
    });
   });
 });
